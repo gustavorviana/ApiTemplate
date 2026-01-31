b21b827a046f586713096545e5d8c27553ba4385
Gustavo Viana <gustavorviana@outlook.com>
2026-01-31 11:07:46 -0300

    Added swagger

--- Src/ApiTemplate.Api/Program.cs
+++ Src/ApiTemplate.Api/Program.cs
@@ -7,8 +7,9 @@ builder.AddServiceDefaults();
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -17,7 +18,8 @@ app.MapDefaultEndpoints();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
