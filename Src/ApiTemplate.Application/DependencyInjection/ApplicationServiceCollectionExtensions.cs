984fb92efee429b6e77d7d13cd49531a746b623b
Gustavo Viana <gustavorviana@outlook.com>
2026-01-31 10:58:26 -0300

    Add project files.

---
+++ Src/ApiTemplate.Application/DependencyInjection/ApplicationServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace ApiTemplate.Application.DependencyInjection;
+
+public static class ApplicationServiceCollectionExtensions
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        // services.AddScoped<CreateUserUseCase>();
+        // services.AddValidatorsFromAssembly(...);
+        return services;
+    }
+}
---
+++ Src/ApiTemplate.Infrastructure/DependencyInjection/ApiServiceCollectionExtensions.cs
@@ -0,0 +1,39 @@
+using ApiTemplate.Application.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace ApiTemplate.Infrastructure.DependencyInjection;
+
+// Adds common Aspire services: service discovery, resilience, health checks, and OpenTelemetry.
+// This project should be referenced by each service project in your solution.
+// To learn more about using this project, see https://aka.ms/dotnet/aspire/service-defaults
+public static class ApiServiceCollectionExtensions
+{
+    public static TBuilder AddServiceDefaults<TBuilder>(this TBuilder builder) where TBuilder : IHostApplicationBuilder
+    {
+        builder.ConfigureOpenTelemetry();
+
+        builder.AddDefaultHealthChecks();
+
+        builder.Services.AddApplication();
+
+        builder.Services.AddServiceDiscovery();
+
+        builder.Services.ConfigureHttpClientDefaults(http =>
+        {
+            // Turn on resilience by default
+            http.AddStandardResilienceHandler();
+
+            // Turn on service discovery by default
+            http.AddServiceDiscovery();
+        });
+
+        // Uncomment the following to restrict the allowed schemes for service discovery.
+        // builder.Services.Configure<ServiceDiscoveryOptions>(options =>
+        // {
+        //     options.AllowedSchemes = ["https"];
+        // });
+
+        return builder;
+    }
+}
